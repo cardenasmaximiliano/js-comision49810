eb74dcfe429926be8e57fc8d8b20aaaf21dfc49c
maximiliano <maxicardenas92@gmail.com>
2023-11-15 12:00:36 -0300

    clase1

---
+++ clase1/js/index.js
@@ -0,0 +1,113 @@
+
+//aca inicia un console.log  => cuando tenemos solamente una
+
+/* /* cuando tenemos mas de una linea de codigo
+console.log("hola");
+console.log("hola");
+console.log("hola");
+console.log("hola");
+
+*/
+
+//variables
+
+// es la manera en como se declara las variables antes del nacimiento de ecmascript 6
+
+// var numero = 10; //NO USES VAR PORQUE TE MATO
+
+//A PARTIR DE ECMA6 nacieron 2 manera de declarar variables => let y const
+
+ //let primerNumero = 1; //una variable la cual nosotros vamos a poder modificar cuando sea necesario;
+
+// console.log(primerNumero);
+
+// primerNumero = 2;
+
+// console.log(primerNumero);
+
+// const CONSTANTE_NUMERO = 4;    //constante_numero 
+
+// console.log(CONSTANTE_NUMERO);
+
+// CONSTANTE_NUMERO = 5;
+
+
+//declaracion incializacion
+
+//let nombre; declarar la variable
+
+//nombre = "maximiliano"; inicializacion
+
+//tipos de datos 
+
+// let numero = 1;// datos de tipo numericos
+
+// let string = "cadena de caracteres"; //datos de tipo cadena de caracter;
+
+// let boolean = true; //false 
+
+
+//tipos de datos numericos => operadores aritmeticos
+// son herramientas fundamentales para la programacion, ya que nos permiten realizacion operaciones matematicas entre variables y valores. 
+
+/*
+    suma = +
+    resta = - 
+    multiplicacion = *
+    division = /
+    resto o modulo = %  
+
+    incremento = ++
+    decrementar= --
+*/
+
+// let numero1 = 5;
+// let numero2 = 5;
+
+// let resultado = numero1 % numero2;
+
+// console.log(resultado)
+
+// let nombre = "Mili";
+// let apellido = "esposito";
+// let vacio = " ";
+
+// let nombreApellido = nombre + vacio + apellido ;
+
+// console.log(nombreApellido);
+
+// console.log("hola soy " + nombre + " "+ apellido + " y fui top 10");
+
+//prompt y alert
+
+//prompt para pedir un dato al usuario
+/*let nombre = prompt("Ingresa tu nombre wachin");
+
+//para reflejar el dato capturado
+alert(nombre);
+*/
+
+let numero1 = parseInt(prompt("ingresa un numero"));
+let numero2 = parseInt(prompt("ingresa otro numero"));
+
+//todo lo que se pase por prompt va a ingresar al programa como tipo string
+
+let resultado = numero1 + numero2;
+
+alert(resultado);
+
+// pidele al usuario ingrese 3 numero. 
+// luego realiza la siguiente operacion
+
+// primero que el numero 1 se sume con numero2
+// que el resultado se multiple por numero3
+//que luego numero2 se divida por numero1 
+
+//todo esto muestre un resultado en un alert
+
+
+
+
+
+
+
